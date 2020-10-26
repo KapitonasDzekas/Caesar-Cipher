1c1326779f189eb7823dbafc0a82c8f7efefecf8
KapitonasDzekas <61228694+KapitonasDzekas@users.noreply.github.com>
2020-10-26 23:42:54 +0200

    Add files via upload

---
+++ CeasarCipherAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeasarCipher
+{
+    public class CeasarCipherAlgorithm
+    {
+        private static char Cipher(char ch, int shift)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)(((ch + shift - offset) % 26) + offset); //26 galimi raktai, nes 26 skirtingos raidės
+        }
+        public static string Encrypt(string text, int shift)
+        {
+            string result = "";
+            foreach (char ch in text)
+            {
+                result += Cipher(ch, shift);
+            }
+            return result;
+        }
+
+        public static string Decrypt(string text, int shift)
+        {
+            return Encrypt(text, 26 - shift);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your message: ");
+            string message = Console.ReadLine();
+            Console.WriteLine("Enter your encryption key (0-26): ");
+            int key = Convert.ToInt32(Console.ReadLine());
+            if (key >= 0 && key <= 26)
+            {
+                string encryptedMessage = Encrypt(message, key);
+                string decryptedMessage = Decrypt(encryptedMessage, key);
+
+                Console.WriteLine("\nMessage: " + message);
+                Console.WriteLine("Key: {0}", key);
+
+                Console.WriteLine("\nEncrypted message: " + encryptedMessage);
+                Console.WriteLine("Decrypted message: " + decryptedMessage + "\n");
+            }
+            else Console.WriteLine("Wrong key.");        
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CeasarCipher;
+
+namespace CeasarCipherTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string message1 = "Hello";
+        string message2 = ".NET uses the Char structure to represent Unicode code points by using UTF-16 encoding.";
+        [TestMethod]
+        public void Test_EncryptMethod()
+        {
+            string res = CeasarCipherAlgorithm.Encrypt(message1, 2);
+            Assert.AreEqual(res, "Jgnnq");
+        }
+        [TestMethod]
+        public void Test_DecryptMethod()
+        {
+            string encryptedText = CeasarCipherAlgorithm.Encrypt(message1, 2);
+            string res = CeasarCipherAlgorithm.Decrypt(encryptedText, 2);
+            Assert.AreEqual(message1, res);
+        }
+        [TestMethod]
+        public void Test_EncryptMethod2()
+        {
+            string res = CeasarCipherAlgorithm.Encrypt(message2, 2);
+            Assert.AreEqual(res, ".PGV wugu vjg Ejct uvtwevwtg vq tgrtgugpv Wpkeqfg eqfg rqkpvu da wukpi WVH-16 gpeqfkpi.");
+        }
+        [TestMethod]
+        public void Test_DecryptMethod2()
+        {
+            string encryptedText = CeasarCipherAlgorithm.Encrypt(message2, 2);
+            string res = CeasarCipherAlgorithm.Decrypt(encryptedText, 2);
+            Assert.AreEqual(message2, res);
+        }
+    }
+}
